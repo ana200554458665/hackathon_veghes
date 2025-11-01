5a6c85a359c8d9623d98de73ecdb759997954a6e
Victor Ilie <victorilie93@gmail.com>
2025-11-01 10:18:23 +0200

    Adaugare joc interactiv de pattern follow

---
+++ path-drag-game/src/app/page.tsx
@@ -0,0 +1,9 @@
+import PathDragGame from "@/components/PathDragGame";
+
+export default function Page() {
+  return (
+    <main className="max-w-[1000px] mx-auto p-4">
+      <PathDragGame />
+    </main>
+  );
+}
